e2702ea4b3fc7b64389986a8342b92c99cded2b4
iozberk <iozberks@gmail.com>
2022-08-25 23:27:40 +0300

    Basic PyTest added

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from joblib import load, dump
+from starlette.responses import FileResponse 
+from fastapi.testclient import TestClient
+
 
 
 app = FastAPI()
-
-irisSavedFile = 'irisSavedModel.joblib'
-clfUploaded = load(irisSavedFile)
+# irisSavedFile = 'irisSavedModel.joblib'
+# clfUploaded = load(irisSavedFile)
 
 @app.get("/")
 def root():
---
+++ test/test_main.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    assert res.status_code == 200
